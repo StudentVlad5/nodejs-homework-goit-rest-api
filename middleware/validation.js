56dd2d307404caf8a61abc2d49af2751f14a615f
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-01-29 21:37:51 +0200

    remove check body

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,8 +1,5 @@
 const validation = (schema) => {
   return (req, res, next) => {
-    if (!req.body) {
-      return res.status(400).json({ message: "missing fields" });
-    }
     const { error } = schema.validate(req.body);
     if (error) {
       return res.status(400).json({ message: error.message });
